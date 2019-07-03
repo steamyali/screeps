6d4861e5dd5c39c013dbf830702ff8714f267164
ljfcnyali <ljfcnyali@gmail.com>
2019-07-03 13:03:32 +0800

    2019.07.03 Update

--- builder.js
+++ builder.js
@@ -16,17 +16,18 @@ var roleBuilder = {
 	        var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
             if(targets.length) {
                 if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                    creep.moveTo(targets[0]);
                 }
             }
 	    }
 	    else {
 	        var sources = creep.room.find(FIND_SOURCES);
             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+                creep.moveTo(sources[0]);
             }
 	    }
 	}
 };
 
 module.exports = roleBuilder;
+
