9025abf07389c8504b4041ddff47c4fb6d86bc48
ljfcnyali <ljfcnyali@gmail.com>
2019-07-03 12:53:26 +0800

    2019.07.03 Update

--- builder.js
+++ builder.js
@@ -5,11 +5,11 @@ var roleBuilder = {
 
 	    if(creep.memory.building && creep.carry.energy == 0) {
             creep.memory.building = false;
-            creep.say('🔄 harvest');
+            creep.say('harvest');
 	    }
 	    if(!creep.memory.building && creep.carry.energy == creep.carryCapacity) {
 	        creep.memory.building = true;
-	        creep.say('🚧 build');
+	        creep.say('build');
 	    }
 
 	    if(creep.memory.building) {
--- upgrader.js
+++ upgrader.js
@@ -2,7 +2,17 @@ var roleUpgrader = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-	    if(creep.carry.energy < creep.carryCapacity) {
+
+	    if( creep.carry.energy == 0) {
+            creep.memory.upgrading = false;
+            creep.say('harvest');
+	    }
+	    if(creep.carry.energy == creep.carryCapacity) {
+	        creep.memory.upgrading = true;
+	        creep.say('upgrade');
+	    }
+
+	    if(creep.memory.upgrading == false && creep.carry.energy < creep.carryCapacity) {
             var sources = creep.room.find(FIND_SOURCES);
             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[0]);
