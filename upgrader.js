e1a1a5ec4b5a581e4f43de0b098763e440c8e980
ljfcnyali <ljfcnyali@gmail.com>
2019-07-03 12:37:31 +0800

    2019.07.03 Update

--- upgrader.js
+++ upgrader.js
@@ -2,7 +2,7 @@ var roleUpgrader = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-	    if(creep.carry.energy == 0) {
+	    if(creep.carry.energy < creep.carryCapacity) {
             var sources = creep.room.find(FIND_SOURCES);
             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[0]);
