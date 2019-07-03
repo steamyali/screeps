4f9fb6ae0cf215d2bf45471ccf0718bf446f152e
ljfcnyali <ljfcnyali@gmail.com>
2019-07-03 16:07:33 +0800

    2019.07.03 Update

--- harvester.js
+++ harvester.js
@@ -2,13 +2,34 @@ var roleHarvester = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-	    if(creep.carry.energy < creep.carryCapacity) {
+
+	    if(creep.carry.energy == 0) {
+            creep.memory.carrying = false;
+            creep.say('harvest');
+	    }
+	    if(creep.carry.energy == creep.carryCapacity) {
+	        creep.memory.carrying = true;
+	        creep.say('finish');
+	    }
+
+	    if(creep.memory.carrying == false) {
             var sources = creep.room.find(FIND_SOURCES);
             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[0]);
             }
         }
         else {
+            const targets = creep.room.find(FIND_STRUCTURES, {
+                filter: object => object.hits < object.hitsMax});
+
+            targets.sort((a,b) => a.hits - b.hits);
+
+            if(targets.length > 0) {
+                if(creep.repair(targets[0]) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0]);
+                }
+            }
+
             if(creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(Game.spawns['Spawn1']);
             }
