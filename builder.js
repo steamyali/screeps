84a107f2b16b66f2b8f5e97e4a599c6c405cd6b1
ljfcnyali <ljfcnyali@gmail.com>
2019-07-03 13:36:26 +0800

    2019.07.03 Update

--- builder.js
+++ builder.js
@@ -3,29 +3,29 @@ var roleBuilder = {
     /** @param {Creep} creep **/
     run: function(creep) {
 
-	    if(creep.memory.building && creep.carry.energy == 0) {
+	    if(creep.carry.energy == 0) {
             creep.memory.building = false;
             creep.say('harvest');
 	    }
-	    if(!creep.memory.building && creep.carry.energy == creep.carryCapacity) {
+	    if(creep.carry.energy == creep.carryCapacity) {
 	        creep.memory.building = true;
 	        creep.say('build');
 	    }
 
-	    if(creep.memory.building) {
+	    if(creep.memory.upgrading == false) {
+            var sources = creep.room.find(FIND_SOURCES);
+            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0]);
+            }
+        }
+        else { 
 	        var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
             if(targets.length) {
                 if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0]);
                 }
             }
-	    }
-	    else {
-	        var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0]);
-            }
-	    }
+        }
 	}
 };
 
--- upgrader.js
+++ upgrader.js
@@ -3,7 +3,7 @@ var roleUpgrader = {
     /** @param {Creep} creep **/
     run: function(creep) {
 
-	    if( creep.carry.energy == 0) {
+	    if(creep.carry.energy == 0) {
             creep.memory.upgrading = false;
             creep.say('harvest');
 	    }
@@ -12,7 +12,7 @@ var roleUpgrader = {
 	        creep.say('upgrade');
 	    }
 
-	    if(creep.memory.upgrading == false && creep.carry.energy < creep.carryCapacity) {
+	    if(creep.memory.upgrading == false) {
             var sources = creep.room.find(FIND_SOURCES);
             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[0]);
