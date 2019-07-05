db56b6efae2149630aad07c4c1789ee2c17b96dd
yalikiwi <ykwcnyali@gmail.com>
2019-07-05 20:37:40 +0800

    2019.07.05 Update

---
+++ attacker.js
@@ -0,0 +1,24 @@
+var roleAttacker = {
+	
+	//攻击者，士兵(I don't kone how to explain it...)
+	//featur: [ATTACK,ATTACK,MOVE,MOVE，MOVE,MOVE,MOVE,MOVE,TOUCH,TOUCH],成本370
+	run: function(creep) {
+
+		const target = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+		
+		if(target) {
+    		if(creep.attack(target) == ERR_NOT_IN_RANGE) {
+        	creep.moveTo(target);
+    		}
+		}
+		//自动寻找敌人并接近
+
+		if(creep.hit*100 <= creep.hitsMax*30){
+			var tower = Game.getObjectById('5d1d9a478602d7647c75fab9'); 
+			creep.moveTo(tower);
+		}
+		//血量处于危险状态时回塔加血
+		
+		
+	}
+}
