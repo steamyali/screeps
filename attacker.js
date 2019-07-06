82f2f4d8ffde5d0f1e774c661c9523bb75576e8d
ljfcnyali <ljfcnyali@gmail.com>
2019-07-06 16:03:13 +0800

    2019.07.06 Update

--- attacker.js
+++ attacker.js
@@ -1,9 +1,20 @@
 var roleAttacker = {
 	
 	//攻击者，士兵(I don't kone how to explain it...)
-	//featur: [TOUGH,TOUGH,MOVE,MOVE,ATTACK,MOVE,ATTACK,MOVE],成本370
+	//featur: [ATTACK,ATTACK,MOVE,MOVE,MOVE,MOVE,TOUCH,TOUCH],成本370
 	run: function(creep) {
 
+        if ( creep.room.name != 'W11N3' ) {
+            // 这一段是还在赶往另外一个房间的路上
+            creep.say('move');
+            const route = Game.map.findRoute(creep.room, 'W11N3');
+            // 移动到隔壁房间
+            if(route.length > 0) {
+                const exit = creep.pos.findClosestByRange(route[0].exit);
+                creep.moveTo(exit);
+            }
+        }
+            
 		const target = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
 		
 		if(target) {
@@ -12,13 +23,13 @@ var roleAttacker = {
     		}
 		}
 		//自动寻找敌人并接近
-
+/*
 		if(creep.hit*100 <= creep.hitsMax*30){
 			var tower = Game.getObjectById('5d1d9a478602d7647c75fab9'); 
 			creep.moveTo(tower);
 		}
 		//血量处于危险状态时回塔加血
-		
+*/		
 	}
 }
 
