cfae9d576a89aab3729df1731027160a9d000449
ljfcnyali <ljfcnyali@gmail.com>
2019-07-07 20:54:29 +0800

    2019.07.07 Update

--- builder.js
+++ builder.js
@@ -19,11 +19,6 @@ var roleBuilder = {
         const target = creep.pos.findClosestByRange(FIND_CONSTRUCTION_SITES);
         // hits越小优先级越高
 
-        const roads = creep.room.find(FIND_STRUCTURES, {
-            filter: object => ( object.hits < object.hitsMax && object.structureType == STRUCTURE_ROAD )});
-        // 寻找类型为STRUCTURE_ROAD，hits < hitsMax的所有ROAD，这里返回的是一个数组
-        roads.sort((a,b) => a.hits - b.hits);
-
 	    if(creep.memory.building == false) {
             var sources = creep.room.find(FIND_SOURCES);
             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
@@ -34,12 +29,6 @@ var roleBuilder = {
             // 第一优先级为修建建筑
             if(creep.build(target) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(target);
-            }
-        }
-        else if(roads.length > 0) {
-            // 我们认为修复ROAD的优先级比储存资源更高(其实修复ROAD只需要很短的时间即可)
-            if(creep.repair(roads[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(roads[0]);
             }
         }
         else { 
