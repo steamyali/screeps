6c47a3d0a453c3876f52d9e29e88d52a80a3d609
ljfcnyali <ljfcnyali@gmail.com>
2019-07-08 11:28:20 +0800

    2019.07.08 Update

--- builder.js
+++ builder.js
@@ -6,18 +6,17 @@ var roleBuilder = {
     run: function(creep) {
 
         // 跟其它的同理
-	    if(creep.carry.energy == 0) {
+	    if(creep.memory.building && creep.carry.energy == 0) {
             creep.memory.building = false;
             creep.say('harvest');
 	    }
-	    if(creep.carry.energy == creep.carryCapacity) {
+	    if(!creep.memory.building && creep.carry.energy == creep.carryCapacity) {
 	        creep.memory.building = true;
 	        creep.say('build');
 	    }
 
         // 找到最近的待修建的建筑
-        const target = creep.pos.findClosestByRange(FIND_CONSTRUCTION_SITES);
-        // hits越小优先级越高
+        var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
 
 	    if(creep.memory.building == false) {
             var sources = creep.room.find(FIND_SOURCES);
@@ -25,10 +24,10 @@ var roleBuilder = {
                 creep.moveTo(sources[0]);
             }
         }
-        else if(target) {
+        else if(targets.length) {
             // 第一优先级为修建建筑
-            if(creep.build(target) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(target);
+             if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(targets[0]);
             }
         }
         else { 
